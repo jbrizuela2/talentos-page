0952aef9f0c7fab85789843d94a764702c207550
Juan Brizuela <juan.brizuela@michelada.io>
2019-02-07 08:11:29 -0600

    Initial commit

---
+++ lenin.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+  $('#lenin').mouseenter(function(){
+    $('#lenin').text(" ͡° ͜ʖ ͡°");
+  })
+
+  $('#lenin').mouseleave(function(){
+    $('#lenin').text(" ͡° ͟ʖ ͡°");
+  })
+});
